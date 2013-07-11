0e08a51fab017c0a96e282b5c5dfb654594205d7
Jamie Reilly <=>
2013-07-11 23:04:48 +0200

    reading into a buffer

--- web.js
+++ web.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var message;
+
+fs.readFile('index.html', function (err, data) {
+  if (err) throw err;
+  message=data;
+});
 
 app.get('/', function(request, response) {
-  response.send('Hello World! 2 2');
+  response.send(message);
 });
 
 var port = process.env.PORT || 5000;
